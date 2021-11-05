28069854d909d7ca914fe515c891f6140ad87560
Chapdel KAMGA <drop@chapdel.me>
2021-11-05 12:56:58 +0100

    First

---
+++ src/Credit.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Chapdel\Credit;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Credit extends Model
+{
+    public $guarded = [
+        'id'
+    ];
+}
---
+++ src/CreditServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Chapdel\Credit;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Chapdel\Credit\Commands\CreditCommand;
+
+class CreditServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package
+            ->name('laravel-credit')
+            ->hasConfigFile()
+            ->hasMigration('create_credit_table');
+    }
+}
